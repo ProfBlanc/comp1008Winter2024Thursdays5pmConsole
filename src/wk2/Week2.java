b232eef0c47f8e252ca724417d9898c88186ccfd
Prof Blanc <ben.blanc@georgebrown.ca>
2024-01-18 17:34:52 -0500

    Initial commit

---
+++ src/wk2/Week2.java
@@ -0,0 +1,20 @@
+package wk2;
+
+public class Week2 {
+
+//    public static void main(String[] args){}
+  //psvm + tab
+public static void main(String[] args) {
+
+    workingWithInputOutputAndDataTypes();
+}
+static void workingWithInputOutputAndDataTypes(){}
+static void example1(){
+    System.out.println("Hello World");
+
+
+    //sout + tab
+    System.out.println("Hello there!");
+
+}
+}
