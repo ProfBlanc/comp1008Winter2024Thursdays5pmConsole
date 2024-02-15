9186b3a1d2b0c3432990cfdce36ebd52783f3d5e
Prof Blanc <ben.blanc@georgebrown.ca>
2024-02-15 17:21:21 -0500

    Feb 15, 2024 Class Code @ 17h20

---
+++ src/wk6/Week6.java
@@ -0,0 +1,36 @@
+package wk6;
+
+import java.util.ArrayList;
+
+public class Week6 {
+
+    public static void main(String[] args) {
+        example1();
+    }
+    static void example1(){
+
+        ArrayList<Integer> nums = new ArrayList<>();
+        ArrayList<Double> doubles = new ArrayList<>(5);
+
+        //add(), remove(), indexOf(), size(), clear(), contains()
+        //add(EXACT data type)
+        //add(index, data)
+        //remove(value), remove(int index)
+        //indexOf(value)
+        //size() number of values
+        // contains(value)  if value is in the arraylist
+        doubles.add(1d);
+
+    }
+    static void example2(){
+        //Create an arraylist of 10 SHORT values
+        //10 random numbers between -100 and 100 (inclusive)
+        //ask the user for a number
+        //determine if the number is found in the arraylist
+            //Yes: output the index
+            // NO: sorry msg
+    }
+    static void example3(){}
+    static void example4(){}
+    static void example5(){}
+}
