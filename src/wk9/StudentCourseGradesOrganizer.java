02b4a6bbb63e3846ab04b1362c7762a6461d235a
Prof Blanc <ben.blanc@georgebrown.ca>
2024-03-14 19:06:26 -0400

    Mar 14, 2024 Class Code @ 19h00

--- src/wk9/StudentCourseGradesOrganizer.java
+++ src/wk9/StudentCourseGradesOrganizer.java
@@ -1,5 +1,6 @@
 package wk9;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,19 +11,46 @@ public class StudentCourseGradesOrganizer {
     public static void main(String[] args) {
         try{
             phase1();
+            phase2();
         }
         catch (Exception e){
             System.err.println(e);
         }
     }
+    static Path path;
+    static Scanner input = new Scanner(System.in);
     static void phase1() throws IOException {
-        Scanner input = new Scanner(System.in);
+
         Path rootPath = Path.of("src", "wk9", "organizer");
         System.out.println("Enter a year");
         String year = input.nextLine();
         System.out.println("Enter a semester");
         String semester = input.nextLine();
+        path = rootPath.resolve(year).resolve(semester);
+        Files.createDirectories(path);
+    }
+    static void phase2()throws IOException{
+        System.out.println("Enter the number of courses for the semester");
+        int numberOfCourses = input.nextInt();
+        input.nextLine();  // consume \n char
 
-        Files.createDirectories(rootPath.resolve(year).resolve(semester));
+        for(int i = 0; i < numberOfCourses; i++){
+            System.out.println("Enter course code course course#" + (i + 1) );
+            String courseCode = input.nextLine();
+            Files.createDirectory(path.resolve(courseCode));
+        }
+
+        String[] evaluations = {"assignment1", "mid-term"};
+
+        for(File file : path.toFile().listFiles()){
+            for(String evaluation : evaluations){
+                String courseName = file.getName();
+                System.out.printf("Enter the grade for %s of course %s%n", evaluation, courseName);
+                String grade = input.nextLine();
+                Files.writeString(path.resolve(courseName).resolve(evaluation + ".txt"), grade  );
+            }
+        }
+
+
     }
 }
